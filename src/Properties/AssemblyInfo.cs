eb61f26e49ced27172b7214c1b439693a4c4f461
GitHub Action <action@github.com>
2024-01-11 12:45:23 +0000

    (skip): 🔖 bump version to 1.1.5-dev.18

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.2.12")]
-[assembly: AssemblyVersion("1.1.2.12")]
-[assembly: AssemblyFileVersion("1.1.2.12")]
-[assembly: AssemblyInformationalVersion("1.1.2-dev.12+Branch.dev.Sha.9a6b1d1c3733aa9661bb4e00431838d7e458c66e")]
+// [assembly: AssemblyVersion("1.1.5.18")]
+[assembly: AssemblyVersion("1.1.5.18")]
+[assembly: AssemblyFileVersion("1.1.5.18")]
+[assembly: AssemblyInformationalVersion("1.1.5-dev.18+Branch.dev.Sha.675635005a739064f40ddce6471e54581cedd1f6")]
