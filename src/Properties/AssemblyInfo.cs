bc613c21f6b1eefde037609f4f6ee0785dc5f63a
GitHub Action <action@github.com>
2024-01-19 16:31:19 +0000

    (skip): 🔖 bump version to 1.3.0-dev.44

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.42")]
-[assembly: AssemblyVersion("1.3.0.42")]
-[assembly: AssemblyFileVersion("1.3.0.42")]
-[assembly: AssemblyInformationalVersion("1.3.0-dev.42+Branch.dev.Sha.e1b1d0b11d0653f1ddd2a37a8ab4248e94afdb2a")]
+// [assembly: AssemblyVersion("1.3.0.44")]
+[assembly: AssemblyVersion("1.3.0.44")]
+[assembly: AssemblyFileVersion("1.3.0.44")]
+[assembly: AssemblyInformationalVersion("1.3.0-dev.44+Branch.dev.Sha.154e61f0199ab05e433ebef4b016ee06b701e922")]
